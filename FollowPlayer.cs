033c87a2492bc59942ad9f763299a43b92dfd58c
Ivan <34918303+ivanvladimirov@users.noreply.github.com>
2019-01-28 19:57:32 +0200

    Scripts
    All the necessary code/scripts for the game

---
+++ FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    public float smoothSpeed = 0.01f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sideWaysForce = 50f;
+    public float rotationForce = 1;
+    
+    void FixedUpdate()
+    {
+        // Movement
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(sideWaysForce, 0, 0);
+            rb.transform.Rotate(0, -rotationForce, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(-sideWaysForce, 0, 0);
+            rb.transform.Rotate(0, rotationForce, 0);
+        }
+
+        // Turbo Mode
+
+        if(Input.GetKey("space"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime * 2);
+        }
+
+        rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+    }
+}
