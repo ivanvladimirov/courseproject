d71d90ef0c77c058003bf6e09ffe8b8dd6be3a79
Ivan <34918303+ivanvladimirov@users.noreply.github.com>
2019-01-28 19:49:39 +0200

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject completeLevelUI;
+    public float restartDelay = 3.5f;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("RestartGame", restartDelay);
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
